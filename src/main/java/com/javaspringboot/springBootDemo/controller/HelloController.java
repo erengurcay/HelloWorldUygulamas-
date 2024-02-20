3bcf5f19f9df8069905adea02abea12fd767c292
Eren Gurcay <geren.gurcay@istanbulticaret.edu.tr>
2024-02-20 16:21:42 +0300

    Adding RequestMapping

--- src/main/java/com/javaspringboot/springBootDemo/controller/HelloController.java
+++ src/main/java/com/javaspringboot/springBootDemo/controller/HelloController.java
@@ -1,12 +1,19 @@
 package com.javaspringboot.springBootDemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/api")
 public class HelloController {
-    //localhost:8080/hello ---> hello world
-    @GetMapping (path = "/hello")
+    //localhost:8080/api/hello ---> hello world
+
+
+    // @GetMapping (path = "/hello")
+
+    @RequestMapping(path = "/hello", method = RequestMethod.GET)
     public String sayHello(){
         return "Hello World!";
     }
