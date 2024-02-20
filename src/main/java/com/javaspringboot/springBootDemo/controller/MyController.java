da49085575a449c601b05d794d193ebfc4571252
Eren Gurcay <geren.gurcay@istanbulticaret.edu.tr>
2024-02-20 17:04:02 +0300

    RequestBody Annotation

--- src/main/java/com/javaspringboot/springBootDemo/controller/MyController.java
+++ src/main/java/com/javaspringboot/springBootDemo/controller/MyController.java
@@ -1,16 +1,29 @@
 package com.javaspringboot.springBootDemo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.javaspringboot.springBootDemo.model.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api")
 public class MyController {
-    @GetMapping("/message/{m}")
-    public String getMyMessage(@PathVariable("m") String message){
-        return "Your message is : " + message;
+
+//    @GetMapping("/message/{m}")
+//    public String getMyMessage(@PathVariable(name = "m", required = false) String message){
+//       return "Your message is : " + message;
+//   }
+    @PostMapping("users")
+    public User saveUser(@RequestBody User user){
+        System.out.println("User saved!");
+        user.setPassword("");
+        return user;
+    }
+    @PostMapping("all-users")
+    public List<User> saveAllUser(@RequestBody List<User> users){
+        System.out.println("All users saved!");
+        users.forEach(user -> user.setPassword(""));
+        return users;
     }
 
 
